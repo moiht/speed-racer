3bf8c092bbdad49ac9b39c9fd6ce19d59632d3b2
moiht <71550278+moiht@users.noreply.github.com>
2021-11-01 18:39:16 +0530

    Add files via upload

--- js/Player.js
+++ js/Player.js
@@ -4,6 +4,8 @@ class Player {
     this.index = null;
     this.positionX = 0;
     this.positionY = 0;
+    this.rank = 0;
+    this.score = 0;
   }
 
   addPlayer() {
@@ -18,25 +20,28 @@ class Player {
     database.ref(playerIndex).set({
       name: this.name,
       positionX: this.positionX,
-      positionY: this.positionY
+      positionY: this.positionY,
+      rank: this.rank,
+      score: this.score
     });
   }
-  getDistance(){
-  var playerDistanceRef = database.ref("players/player"+this.index)
-  playerDistanceRef.on("value",data =>{
-    var data = data.val();
-    this.positionX = data.positionX;
-    this.positionY = data.positionY; 
-  })
- }
+
+  getDistance() {
+    var playerDistanceRef = database.ref("players/player" + this.index);
+    playerDistanceRef.on("value", data => {
+      var data = data.val();
+      this.positionX = data.positionX;
+      this.positionY = data.positionY;
+    });
+  }
+
   getCount() {
     var playerCountRef = database.ref("playerCount");
     playerCountRef.on("value", data => {
       playerCount = data.val();
     });
   }
-   
-  
+
   updateCount(count) {
     database.ref("/").update({
       playerCount: count
@@ -47,7 +52,9 @@ class Player {
     var playerIndex = "players/player" + this.index;
     database.ref(playerIndex).update({
       positionX: this.positionX,
-      positionY: this.positionY
+      positionY: this.positionY,
+      rank: this.rank,
+      score: this.score
     });
   }
 
@@ -58,4 +65,3 @@ class Player {
     });
   }
 }
-
